828504e2e5167d78e20b36cc13a15261ec0ebf88
Jimmy Flatting <jimmy.flatting@icloud.com>
2025-12-10 13:01:05 +0100

    scripts: add hot-reloading for npm run dev

--- lib/server.ts
+++ lib/server.ts
@@ -1,1 +1,11 @@
-console.log("Server is running...");
+import http from 'http';
+
+const app = http.createServer((req, res) => {
+    res.writeHead(200, { 'Content-Type': 'text/plain' });
+    res.end('Hello, World!!!\n');
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+});
